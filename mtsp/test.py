687755fccfea85fc7a982bbcce8ed63fb31d8589
XiaoMingZHM <zhm1998@outlook.com>
2021-12-01 14:48:23 +0800

    data loader for logs

--- mtsp/test.py
+++ mtsp/test.py
@@ -6,19 +6,20 @@ import matplotlib.pyplot as plt
 import progressbar
 import json
 
+
 def solve(data):
     configs.numNodes = data.size
     # Add Dustbins
     RouteManager.clearDustbin()
     for i in range(configs.numNodes):
-        RouteManager.addDustbin(Dustbin(data.values[i][1],data.values[i][2]))
+        RouteManager.addDustbin(Dustbin(data.values[i][1], data.values[i][2]))
 
     random.seed()
-    fittest = [] # Fittest value (distance)
+    fittest = []  # Fittest value (distance)
 
     pop = Population(configs.populationSize, True)
     globalRoute = pop.getFittest()
-    print ('Initial minimum distance: ' + str(globalRoute.getDistance()))
+    print('Initial minimum distance: ' + str(globalRoute.getDistance()))
 
     # Start evolving
 
@@ -29,16 +30,16 @@ def solve(data):
             globalRoute = localRoute
         fittest.append(localRoute.getDistance())
 
-    print ('Global minimum distance: ' + str(globalRoute.getDistance()))
-    print ('Final Route: ' + globalRoute.toString())
+    print('Global minimum distance: ' + str(globalRoute.getDistance()))
+    print('Final Route: ' + globalRoute.toString())
 
     return fittest
 
 
-if __name__ == '__main__':
+def benchmark():
     result = {}
-
     from data.instances import instance_list, instance
+
     for data_file in instance_list:
         record = {}
         data = instance("../data/" + data_file)
@@ -48,5 +49,29 @@ if __name__ == '__main__':
             record[f"{i}"] = ret
         result[data_file] = record
     with open('test.json', 'w') as f:
-        json.dump(result, f, indent = 4) # 编码JSON数据
+        json.dump(result, f, indent=4)  # 编码JSON数据
 
+
+def data_loader():
+    from data.instances import instance_list
+    data = None
+    with open('test.json', 'r') as f:
+        data = json.load(f)
+    if not data:
+        print("ERROR")
+    for data_file in instance_list:
+        result = data[data_file]
+        last_output = []
+        for value in result.values():
+            last_output.append(value[-1])
+
+        average_output = 0
+        for value in last_output:
+            average_output += value
+        average_output = average_output/len(result)
+        print(last_output)
+        print(data_file, average_output)
+
+if __name__ == '__main__':
+    # benchmark()
+    data_loader()
